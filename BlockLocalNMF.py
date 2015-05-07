466dd312d8ea9a395fc0d1a8a6a3321381cf018c
Johannes Friedrich <j.friedrich@columbia.edu>
2015-05-07 17:06:52 -0400

    changed BlockLocalNMF so that it runs, hopefully even correctly

--- BlockLocalNMF.py
+++ BlockLocalNMF.py
@@ -1,8 +1,7 @@
 from os import getcwd
-from numpy import sum, zeros, reshape, r_, ix_, exp, tile, arange, sqrt, pi, dot
-from functools import reduce
-import operator
+from numpy import sum, zeros, reshape, r_, ix_, exp, arange, sqrt, pi, dot, outer, prod
 import numpy as np
+
 
 def GetHomeFolder():
     # Obtains the folder in which data is saved - change accordingly
@@ -52,139 +51,109 @@ def RegionCut(X, box, *args):
     if len(dims) - 1 != len(box):
         raise Exception('box has the wrong number of dimensions')
     return X[ix_(*map(lambda a: range(*a), box))].reshape((-1, dims[-1]))
-    
-def prod(factors):
-    return reduce(operator.mul, factors, 1)
 
-def LocalNMF(data, centers,activity, sig, NonNegative=False, tol=1e-7, iters=100, verbose=False):
-#        Input:
-#            data - Tx(XxYx...Z) array of the data 
-#            centers - list of L centers of suspected neurons, of length L, where D is spatial dimension (2 or 3)
-#            activity - list of L traces of temporal activity, of length L
-#            sig - size of the gaussian kernel in different spatial directions            
-#          Optional:
-#           NonNegative -  if true, neurons should be considered as non-negative
-#           tol  - tolerance for stopping algorithm
-#           iters - maximum number of iterations
-#           verbose - print progress if true
-#        Output:
-#            MSE_array - Mean square error during algorithm operation
-#            shapes  - the neuronal shape vectors
-#            activity - the neuronal activity for each shape
-#            boxes -  edges of the boxes in which each neuronal shapes lie
 
-# Initialize Parameters
-    dims=data.shape
-    D=len(dims)
-    R=3*np.array(sig) #size of bounding box is 3 times size of neuron
-    L=len(centers)
-    shapes=[]
-    boxes=[]
-    MSE_array=[]
-    residual=data
-    
+def LocalNMF(data, centers, activity, sig, NonNegative=False, tol=1e-7, iters=100, verbose=False):
+    #        Input:
+    #            data - Tx(XxYx...Z) array of the data
+    #            centers - list of L centers of suspected neurons, of length L, where D is spatial dimension (2 or 3)
+    #            activity - list of L traces of temporal activity, of length L
+    #            sig - size of the gaussian kernel in different spatial directions
+    #          Optional:
+    #           NonNegative -  if true, neurons should be considered as non-negative
+    #           tol  - tolerance for stopping algorithm
+    #           iters - maximum number of iterations
+    #           verbose - print progress if true
+    #        Output:
+    #            MSE_array - Mean square error during algorithm operation
+    #            shapes  - the neuronal shape vectors
+    #            activity - the neuronal activity for each shape
+    #            boxes -  edges of the boxes in which each neuronal shapes lie
+
+    # Initialize Parameters
+    dims = data.shape
+    D = len(dims)
+    R = 3 * np.array(sig)  # size of bounding box is 3 times size of neuron
+    L = len(centers)
+    shapes = []
+    boxes = []
+    MSE_array = []
+    residual = data
+
 # Initialize shapes, activity, and residual
     for ll in range(L):
-        boxes.append(GetBox(centers[ll],R,dims))
-        if D>3:
-            xm  = arange(dims[1]).reshape(dims[1],1,1)
-            ym  = arange(dims[2]).reshape(1,dims[2],1)
-            zm  = arange(dims[3]).reshape(1,1,dims[3])
-            
-            temp=exp( -(((xm-centers[ll][0])**2)/(2*sig[0])) \
-            -((ym-centers[ll][1])**2)/(2*sig[1]) \
-            -((zm-centers[ll][2])**2)/(2*sig[2]) )/sqrt(2*pi)/prod(sig);
-            temp=temp.reshape((dims[1],dims[2],dims[3],1))
-            temp=tile(temp,(1, 1, 1, 2)) #just so we can use Region Cut
-        else:
-            xm  = arange(dims[1]).reshape(dims[1],1)
-            ym  = arange(dims[2]).reshape(1,dims[2])
-            
-            temp=exp( -(((xm-centers[ll][0])**2)/(2*sig[0])) \
-            -((ym-centers[ll][1])**2)/(2*sig[1])) /sqrt(2*pi)/prod(sig);
-            temp=temp.reshape((dims[1],dims[2],1))
-            temp=tile(temp,(1, 1, 2)) #just so we can use Region Cut
-            
-        temp=RegionCut(temp,boxes[ll])
-        shapes.append(temp[:,1])
+        boxes.append(GetBox(centers[ll], R, dims))
+        if D > 3:
+            xm = arange(dims[0]).reshape(dims[0], 1, 1)
+            ym = arange(dims[1]).reshape(1, dims[1], 1)
+            zm = arange(dims[2]).reshape(1, 1, dims[2])
 
-        residual=RegionAdd(residual,-dot(reshape(shapes[ll],(len(shapes[ll]),1)),reshape(activity[ll],(1, dims[0])),boxes[ll]))
-    
-## Main Loop
-    
-    for kk in range(iter):
+            temp = exp(-(((xm - centers[ll][0]) ** 2) / (2 * sig[0]))
+                       - ((ym - centers[ll][1]) ** 2) / (2 * sig[1])
+                       - ((zm - centers[ll][2]) ** 2) / (2 * sig[2])) / sqrt(2 * pi) / prod(sig)
+            temp = temp.reshape((dims[1], dims[2], dims[3], 1))
+        else:
+            xm = arange(dims[1]).reshape(dims[1], 1)
+            ym = arange(dims[2]).reshape(1, dims[2])
+
+            temp = exp(-(((xm - centers[ll][0]) ** 2) / (2 * sig[0]))
+                       - ((ym - centers[ll][1]) ** 2) / (2 * sig[1])) / sqrt(2 * pi) / prod(sig)
+            temp = temp.reshape((dims[1], dims[2], 1))
+
+        temp = RegionCut(temp, boxes[ll])
+        shapes.append(temp[:, 0])
+
+        residual = RegionAdd(
+            residual, -outer(shapes[ll], activity[ll]), boxes[ll])
+
+# Main Loop
+
+    for kk in range(iters):
         for ll in range(L):
             # add region
-            residual=RegionAdd(residual,dot(shapes[ll],activity[ll].T),boxes[ll])            
+            residual = RegionAdd(
+                residual, outer(shapes[ll], activity[ll]), boxes[ll])
 
             # cut region
-            X=RegionCut(residual,boxes[ll]);
-            
-            # NonNegative greedy PCA       
-            greedy_pca_iterations=5;
+            X = RegionCut(residual, boxes[ll])
+
+            # NonNegative greedy PCA
+            greedy_pca_iterations = 5
             for ii in range(greedy_pca_iterations):
-                temp=dot(X.T,shapes[ll])/sum(shapes[ll]**2)
+                temp = dot(X.T, shapes[ll]) / sum(shapes[ll] ** 2)
                 if NonNegative:
-                    temp[temp<0]=0
-                activity[ll]=temp
-                
-                temp=dot(X,activity[ll])/sum(activity[ll]**2)
+                    temp[temp < 0] = 0
+                activity[ll] = temp
+
+                temp = dot(X, activity[ll]) / sum(activity[ll] ** 2)
                 if NonNegative:
-                    temp[temp<0]=0
-                shapes[ll]=temp
-            
+                    temp[temp < 0] = 0
+                shapes[ll] = temp
+
             # Subtract region
-            residual=RegionAdd(residual,-shapes[ll]*activity[ll].T,boxes[ll])
-        
-        # Measure MSE   
-        MSE=sum(residual**2)/prod(dims)
-        if abs(1-MSE/MSE_array[-1])<tol:
+            residual = RegionAdd(
+                residual, -outer(shapes[ll], activity[ll]), boxes[ll])
+
+        # Measure MSE
+        MSE = sum(residual ** 2) / prod(dims)
+        if kk > 0 and abs(1 - MSE / MSE_array[-1]) < tol:
             break
-        if kk==(iter-1):
+        if kk == (iters - 1):
             print('Maximum iteration limit reached')
-        MSE_array.append(MSE);   
+        MSE_array.append(MSE)
         if verbose:
             print('{0:1d}: MSE = {1:.3f}'.format(kk, MSE))
-        
-    
-#    return MSE_array,shapes,activity,boxes
-    
-T=50; X=200; Y=100
-data=np.random.randn(T,X,Y)
-# centers=[[ 40, 30]]
-centers=[[40, 30]]
-activity=[np.random.randn(T)]
-sig = [3, 3]
-R=3*np.array(sig) 
-dims=data.shape
-LocalNMF(data, centers,activity, sig, NonNegative=True)
-    
-# test python 3d:
-#Z = np.ones((3, 4, 5, 5))
 
-#X = np.arange(40).reshape((5, 8)).T
-#box = np.array([[0, 2], [1, 3], [2, 4]])
-#RegionAdd(Z, X, box)
-#print(Z[0, 1, 2, 3])
-# print RegionCut(Z,box)
-# test python 2d:
-# Z = np.ones((6, 4, 5))
-# X = np.arange(45).reshape((5, 9)).T
-# box = np.array([[0, 3], [1, 4]])
-# RegionAdd(Z, X, box)
-# print Z[0, 1, 2]
-# print RegionCut(Z,box)
-# test matlab 3d:
-# Z = ones(3, 4, 5, 5);
-# X = reshape(0: 39, [8, 5]);
-# box = [1, 2; 2, 3; 3, 4];
-# Z = RegionAdd(Z, X, box);
-# display(Z(1, 2, 3, 4));
-# display(RegionCut(Z,box));
-# test matlab 2d:
-# Z = ones(6, 4, 5);
-# X = reshape(0: 44, [9, 5]);
-# box = [1, 3; 2, 4];
-# Z = RegionAdd(Z, X, box);
-# display(tmp(1, 2, 3));
-# display(RegionCut(Z,box));
+    return MSE_array, shapes, activity, boxes
+
+
+T = 50
+X = 200
+Y = 100
+data = np.random.randn(X, Y, T)
+centers = [[40, 30]]
+activity = [np.random.randn(T)]
+sig = [3, 3]
+R = 3 * np.array(sig)
+dims = data.shape
+LocalNMF(data, centers, activity, sig, NonNegative=True)
